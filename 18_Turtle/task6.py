7e707ac5172256afc90798b1685a1a44e05dac2f
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-04-25 21:43:49 +0900

    complete

--- 18_Turtle/task6.py
+++ 18_Turtle/task6.py
@@ -1,6 +1,8 @@
 import colorgram
+import turtle as t
+import random
 
-colors = colorgram.extract('18_Turtle/img.jpg',50)
+colors = colorgram.extract('img.jpg',50)
 
 color_list = []
 for color in colors:
@@ -12,5 +14,18 @@ for color in colors:
     color_tuple = (r, g, b)
     color_list.append(color_tuple)
 
-print(color_list)
-    
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+t.colormode(255)
+# t.setup(width=500, height=500)
+
+for idx in range(101):
+
+    tim.dot(20, random.choice(color_list))
+    # tim.penup()
+    tim.fd(50)
+
+    if idx % 10 == 0:
+        tim.setpos(0, idx*5)
