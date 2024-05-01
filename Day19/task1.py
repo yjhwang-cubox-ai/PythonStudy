4a27873fa1e14ae52058c17a39853087b5a3b6b2
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-05-01 16:56:12 +0900

    day20-task1

--- Day19/task1.py
+++ Day19/task1.py
@@ -8,9 +8,25 @@ def move_forwards():
 
 def move_backwards():
     tim.backward(10)
+    
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
 
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+    
+def clean():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="c", fun=clean)
 screen.exitonclick()
---
+++ Day20/task1.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My snake game")
+
+
+screen.exitonclick()
