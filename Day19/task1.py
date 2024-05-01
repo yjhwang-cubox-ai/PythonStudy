55dd4967ef9630d197a01a7606f6c0eb3155348f
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-05-01 13:56:15 +0900

    task1

--- Day19/task1.py
+++ Day19/task1.py
@@ -6,6 +6,11 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
 screen.exitonclick()
