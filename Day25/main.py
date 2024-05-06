1c862de32f4fc9235c0b7d2539bbe1e61130a978
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-05-06 17:39:10 +0900

    finish day25

--- Day25/main.py
+++ Day25/main.py
@@ -1,11 +1,41 @@
-import csv
+import turtle
+import pandas
 
-with open("Day25/weather_data.csv") as file:
-    content = csv.reader(file)
-    temperature = []
-    for row in content:
-        if row[1] == "temp":
+def main():
+    df = pandas.read_csv("Day25/50_states.csv")
+    print(df)
+    
+    screen = turtle.Screen()
+    screen.title("U.S. States Game")
+    image = "us-states-game-start\\blank_states_img.gif"
+    screen.addshape(image)
+    turtle.shape(image)
+    
+    while True:
+        answer_state = screen.textinput(title="US states", prompt="input states").title()
+        
+        if answer_state == "Exit":
+            df.to_csv("study.csv")
+            break        
+        
+        user_select = df[df.state == answer_state]
+                
+        if user_select.empty:
             continue
-        temperature.append(int(row[1]))
+        
+        print(user_select)
+        
+        x_cor = int(user_select.x.item())
+        y_cor = int(user_select.y.item())
+        
+        point = turtle.Turtle()
+        point.hideturtle()
+        point.penup()
+        point.goto(x_cor,y_cor)
+        point.write(user_select.state.item(), align="center")
+        
+        df.drop(user_select.index,  axis=0, inplace=True)
+    
 
-print(temperature)
+if __name__ == "__main__":
+    main()
