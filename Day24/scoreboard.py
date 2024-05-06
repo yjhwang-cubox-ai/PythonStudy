b9310ba296f479800a5853014d45f351cde0b7ca
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-05-06 15:23:49 +0900

    finish Day24

--- Day24/scoreboard.py
+++ Day24/scoreboard.py
@@ -7,7 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_highscore()
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -27,3 +27,13 @@ class Scoreboard(Turtle):
             self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
+        self.update_highscore(self.high_score)
+    
+    def read_highscore(self):
+        with open("0506/data.txt", mode="r") as file:
+            high_score = file.read()
+        return int(high_score)
+
+    def update_highscore(self, new_highscore):
+        with open("0506/data.txt", mode="w") as file:
+            file.write(str(new_highscore))
