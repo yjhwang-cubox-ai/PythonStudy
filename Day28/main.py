5fee0baa7d375c347b9be8f5299f1ee85a435caf
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-05-15 17:59:07 +0900

    Day28 finish

--- Day28/main.py
+++ Day28/main.py
@@ -6,20 +6,28 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
 REPS = 0
+TIMER = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    window.after_cancel(TIMER)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_label.config(text='')
+    global REPS
+    REPS = 0
+    
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global REPS
     REPS += 1
     
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
+    work_sec = WORK_MIN * 5
+    short_break_sec = SHORT_BREAK_MIN * 3
     long_break_sec = LONG_BREAK_MIN * 60
     
     if REPS % 8 == 0:
@@ -27,12 +35,16 @@ def start_timer():
         count_down(long_break_sec)
         
     if REPS % 2 == 1:
-        title_label.config(text="Work Time", fg=GREEN)
+        title_label.config(text="Work", fg=GREEN)
         count_down(work_sec)
         
     if REPS % 2 == 0:
-        title_label.config(text="Short Break", fg=PINK)
+        title_label.config(text="Break", fg=PINK)
         count_down(short_break_sec)
+        
+    
+    
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
@@ -45,9 +57,16 @@ def count_down(count):
     
     canvas.itemconfig(timer_text, text=updated_count_text)
     if count > 0:
-        window.after(1000, count_down, count-1)
+        global TIMER
+        TIMER = window.after(1000, count_down, count-1)
     else:
         start_timer()
+        mark = ""
+        global REPS
+        for _ in range(math.floor(REPS/2)):
+            mark += '✔'
+        check_label.config(text=mark)
+            
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -71,11 +90,11 @@ canvas.grid(column=1, row=1)
 button_start = Button(text="Start", highlightthickness=0, command=start_timer)
 button_start.grid(column=0, row=2)
 
-button_reset = Button(text="Reset", highlightthickness=0)
+button_reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
 button_reset.grid(column=2, row=2)
 
 #check
-check_label = Label(text="✔", font=(15),fg=GREEN, bg=YELLOW)
+check_label = Label(font=(15),fg=GREEN, bg=YELLOW)
 check_label.grid(column=1, row=3)
 
 window.mainloop()
